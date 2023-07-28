226cea53f147dd280ecaa90ac02f8022f959cb41
AÏT-AMMAR Vincent <vincent.ait-ammar@developpement-durable.gouv.fr>
2023-07-28 10:31:59 +0200

    dataframe interactive display

---
+++ custom_settings.py
@@ -0,0 +1,15 @@
+from itables import init_notebook_mode
+import itables.options as opt
+from itables import show
+
+def apply_itable_custom_settings():
+    """ Allows interactive dataframe presentation
+    vie the show function.
+    """
+    
+    init_notebook_mode(all_interactive=False, connected=True)
+
+    opt.lengthMenu = [5, 10]
+    opt.style = 'table-layout:auto;width:auto;margin:auto;caption-side:bottom'
+    opt.classes = 'display compact'
+    opt.scrollY = '300px'
